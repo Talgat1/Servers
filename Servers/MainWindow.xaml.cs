54332a0a911810c7d329bdaf182f576130f6a218
arullintalgat36@gmail.com <arullintalgat36@gmail.com>
2022-06-03 10:24:30 +0300

    Create DataBase Servers_prod. Add authorization.

--- Servers/MainWindow.xaml.cs
+++ Servers/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Servers.db;
 
 namespace Servers
 {
@@ -20,9 +21,40 @@ namespace Servers
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Server_prodEntities db = new Server_prodEntities();
+
+        public static User authUser;
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Button_Click_Login(object sender, RoutedEventArgs e)
+        {
+            //seetovar st = new seetovar();
+            //mainadmin ac = new mainadmin();
+            if(LoginTB.Text == "" || PasswordTB.Text == "")
+            {
+                MessageBox.Show($"Ошибка, заполните все поля.");
+            }
+            else
+            {
+                foreach (var user in db.User)
+                {
+                    if (user.Login == LoginTB.Text.Trim() && user.Password == PasswordTB.Text.Trim())
+                    {
+                        MessageBox.Show($"Привет Пользователь {user.Login}");
+                        authUser = user;
+                        ServerWindow sw = new ServerWindow();
+                        this.Close();
+                        sw.Show();
+                    }
+                }
+            }            
         }
     }
 }
